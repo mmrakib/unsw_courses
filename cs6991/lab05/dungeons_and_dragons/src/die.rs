3a9654f0baf1e9db4c4771e032596c12284e6243
mmrakib <mohammadmrakib@gmail.com>
2024-10-25 09:12:12 +1100

    Finished cs6991 lab05 dungeons_and_dragons

--- cs6991/lab05/dungeons_and_dragons/src/die.rs
+++ cs6991/lab05/dungeons_and_dragons/src/die.rs
@@ -22,6 +22,33 @@ pub struct Coin;
 
 // MODIFY/ADD BELOW HERE ONLY
 
-pub fn roll<T>(item: T) -> u8 {
-    todo!()
+// Define a trait for rollable items
+pub trait Rollable {
+    fn roll_value(&self) -> u8;
+}
+
+// Implement the Rollable trait for Die
+impl Rollable for Die {
+    fn roll_value(&self) -> u8 {
+        match self {
+            Die::D4 => get_random_value(4),
+            Die::D6 => get_random_value(6),
+            Die::D8 => get_random_value(8),
+            Die::D10 => get_random_value(10),
+            Die::D12 => get_random_value(12),
+            Die::D20 => get_random_value(20),
+        }
+    }
+}
+
+// Implement the Rollable trait for Coin
+impl Rollable for Coin {
+    fn roll_value(&self) -> u8 {
+        get_random_value(2) // A coin flip results in 1 or 2
+    }
+}
+
+// Generic roll function that works for any Rollable type
+pub fn roll<T: Rollable>(item: T) -> u8 {
+    item.roll_value()
 }
